156e77dcce71cd8467ea7b0bfd614bf07eb7bbdd
cjiong <416137318@qq.com>
2016-05-04 00:24:56 +0800

    added project02 - CustomFonts

---
+++ Project 02 - CustomFonts/Project 02 - CustomFonts/ViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  Project 02 - CustomFonts
+//
+//  Created by 陈炯 on 16/5/3.
+//  Copyright © 2016年 Jiong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
+    
+    var data = ["生活不止眼前的苟且", "还有诗和远方的田野"]
+    
+    let fontNames = ["MFJinHei_Noncommercial-Regular", "MFTongXin_Noncommercial-Regular", "MFTongXin_Noncommercial-Regular1"]
+    
+    var fontRowIndex = 0
+
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var fontTableView: UITableView!
+    @IBOutlet weak var changeFontButton: UIButton!
+    @IBAction func changeFont(sender: AnyObject) {
+        
+        fontRowIndex = (fontRowIndex + 1) % 3
+        fontTableView.reloadData()
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //设置代理和数据源为self
+        fontTableView.delegate = self
+        fontTableView.dataSource = self
+        
+        //改变字体
+        for family in UIFont.familyNames() {
+            for font in UIFont.fontNamesForFamilyName(family) {
+                print(font)
+            }
+        }
+        
+        //改变按钮样式
+        changeFontButton.layer.cornerRadius = 55
+        
+    }
+    
+    //实现数据源协议
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    //返回行高
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 50
+    }
+    //设置单元格样式
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = fontTableView.dequeueReusableCellWithIdentifier("FontCell", forIndexPath: indexPath)
+        let text = data[indexPath.row]
+        
+            cell.textLabel?.text = text
+            cell.textLabel?.textColor = UIColor.whiteColor()
+            cell.textLabel?.font = UIFont(name: self.fontNames[fontRowIndex], size: 16)
+        
+        return cell
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Project 02 - CustomFonts/Project 02 - CustomFontsTests/Project_02___CustomFontsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Project_02___CustomFontsTests.swift
+//  Project 02 - CustomFontsTests
+//
+//  Created by 陈炯 on 16/5/3.
+//  Copyright © 2016年 Jiong. All rights reserved.
+//
+
+import XCTest
+@testable import Project_02___CustomFonts
+
+class Project_02___CustomFontsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
