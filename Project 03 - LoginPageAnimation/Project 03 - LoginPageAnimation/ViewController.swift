e02425eda5d0aad4a24ac4f08dfb40e2e95ad928
cjiong <416137318@qq.com>
2016-05-11 00:23:54 +0800

    -

--- Project 03 - LoginPageAnimation/Project 03 - LoginPageAnimation/ViewController.swift
+++ Project 03 - LoginPageAnimation/Project 03 - LoginPageAnimation/ViewController.swift
@@ -9,15 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func backButtonDidToch(sender: AnyObject) {
+       self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
