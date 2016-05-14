e38c56744e6529a683d64b578af7e362fb34fef9
cjiong <416137318@qq.com>
2016-05-14 16:57:44 +0800

    😏 😏 😏

--- Project 05 - PullToRefresh/Project 05 - PullToRefresh/ViewController.swift
+++ Project 05 - PullToRefresh/Project 05 - PullToRefresh/ViewController.swift
@@ -8,13 +8,84 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    let cellIdentifer = "NewCellIdentifer"
+    
+    let originEmoji = ["😏😏😏", "😂😂😂", "😘😘😘"]
+    let newEmoji = ["🌚🌚🌚", "😱😱😱", "😳😳😳", "😄😄😄"]
+    var emojiData = [String]()
+    var tableViewController = UITableViewController(style: .Plain)
+    
+    var refreshControl = UIRefreshControl()
+    var navigationBar: UINavigationBar = UINavigationBar(frame: CGRect(x: 0, y: 0, width: 375, height: 64))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        emojiData = originEmoji
+        let emojiTableView = tableViewController.tableView
+        
+        emojiTableView.backgroundColor = UIColor.grayColor()
+        emojiTableView.delegate = self
+        emojiTableView.dataSource = self
+        
+        emojiTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: cellIdentifer)
+        
+        tableViewController.refreshControl = self.refreshControl
+        self.refreshControl.addTarget(self, action: "didRoadEmoji", forControlEvents: .ValueChanged)
+        
+        self.refreshControl.backgroundColor = UIColor.brownColor()
+        let attributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+        self.refreshControl.attributedTitle = NSAttributedString(string: "最近一次加载\(NSDate())", attributes: attributes)
+        self.refreshControl.tintColor = UIColor.blueColor()
+        
+        self.title = "emoji"
+        self.navigationBar.barStyle = UIBarStyle.BlackTranslucent
+        
+        emojiTableView.rowHeight = UITableViewAutomaticDimension
+        emojiTableView.estimatedRowHeight = 60.0
+        emojiTableView.tableFooterView = UIView(frame: CGRectZero)
+        emojiTableView.separatorStyle = UITableViewCellSeparatorStyle.None
+        
+        self.view.addSubview(emojiTableView)
+        self.view.addSubview(navigationBar)
+        
     }
 
+    
+    //数据源协议
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojiData.count
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifer)! as UITableViewCell
+        
+        cell.textLabel!.text = self.emojiData[indexPath.row]
+        cell.textLabel!.textAlignment = NSTextAlignment.Center
+        cell.textLabel?.font = UIFont(name: "systemFont", size: 50)
+        cell.backgroundColor = UIColor.clearColor()
+        cell.selectionStyle = UITableViewCellSelectionStyle.None
+        
+        return cell
+        
+    }
+    
+    func didRoadEmoji() {
+        self.emojiData = newEmoji
+        self.tableViewController.tableView.reloadData()
+        self.refreshControl.endRefreshing()
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
