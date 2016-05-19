387e4b0470ce24fba83abb81bcc6000a4d205455
cjiong <416137318@qq.com>
2016-05-19 21:01:47 +0800

    create Project 07 - DidiTaxi

---
+++ Project 07 - DidiTaxi/Project 07 - DidiTaxiTests/Project_07___DidiTaxiTests.swift
@@ -0,0 +1,36 @@
+//
+//  Project_07___DidiTaxiTests.swift
+//  Project 07 - DidiTaxiTests
+//
+//  Created by 陈炯 on 16/5/19.
+//  Copyright © 2016年 Jiong. All rights reserved.
+//
+
+import XCTest
+@testable import Project_07___DidiTaxi
+
+class Project_07___DidiTaxiTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
