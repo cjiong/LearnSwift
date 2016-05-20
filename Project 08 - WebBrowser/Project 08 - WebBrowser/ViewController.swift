3eacbc53e518c35bd71364a737caacec5920b570
cjiong <416137318@qq.com>
2016-05-20 22:36:00 +0800

    complete Project 08 - WebBrowser

---
+++ Project 08 - WebBrowser/Project 08 - WebBrowser/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  Project 08 - WebBrowser
+//
+//  Created by 陈炯 on 16/5/20.
+//  Copyright © 2016年 Jiong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIWebViewDelegate, UITextFieldDelegate {
+
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var webView1: UIWebView!
+    @IBOutlet weak var loading: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //设置代理
+        textField1.delegate = self
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        loadUrl(textField.text!, web: webView1)
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    //加载请求的方法
+    func loadUrl(url: String, web: UIWebView) {
+        
+        //载入输入的请求
+        let aUrl = NSURL(string: ("https://" + url))
+        
+        //解除textField的第一响应者的注册资格，键盘消失；若没有这一步，键盘会一直留在屏幕内
+        let urlRequest = NSURLRequest(URL: aUrl!)
+        web.loadRequest(urlRequest)
+        
+    }
+    
+    //网页开始加载
+    func webViewDidStartLoad(webView: UIWebView) {
+        //轮圈开始转动
+        loading.startAnimating()
+        
+        //状态栏的网络请求轮圈开始转动
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+    }
+    
+    //网页加载结束
+    func webViewDidFinishLoad(webView: UIWebView) {
+        
+        loading.stopAnimating()
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
