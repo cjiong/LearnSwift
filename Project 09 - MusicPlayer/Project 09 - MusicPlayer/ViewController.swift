6272095f11f601e9102fae2e23acbf3820871728
cjiong <416137318@qq.com>
2016-05-22 23:54:56 +0800

    play music

--- Project 09 - MusicPlayer/Project 09 - MusicPlayer/ViewController.swift
+++ Project 09 - MusicPlayer/Project 09 - MusicPlayer/ViewController.swift
@@ -7,17 +7,76 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var playButon: CustomButton!
+    @IBOutlet weak var bgImage: UIImageView!
+    @IBOutlet weak var songLabel: UILabel!
+    @IBOutlet weak var artistLabel: UILabel!
+    @IBOutlet weak var albumImage: UIImageView!
+    
+    var audioPlayer = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //背景模糊效果
+        bgImage.image = UIImage(named: "COA")
+        let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.Light)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = view.bounds
+        bgImage.addSubview(blurEffectView)
+        
+        
+        //歌曲信息
+        albumImage.image = UIImage(named: "fun")
+        albumImage.layer.cornerRadius = 100
+        
+        songLabel.textColor = UIColor.grayColor()
+        songLabel.text = "All Alone"
+        artistLabel.textColor = UIColor.grayColor()
+        artistLabel.text = "Fun."
+        
+        playButon.setImage(UIImage(named: "Play"), forState: UIControlState.Normal)
+        
     }
 
+    //播放音乐
+    @IBAction func playMusicButtonDidTouch(sender: CustomButton) {
+        
+        let song = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Fun. - All Alone", ofType: "mp3")!)
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+            try AVAudioSession.sharedInstance().setActive(true)
+            try audioPlayer = AVAudioPlayer(contentsOfURL: song)
+            
+            audioPlayer.prepareToPlay()
+            audioPlayer.play()
+        }
+        catch let playError as NSError {
+            print(playError)
+        }
+        if sender.isPlay {
+            audioPlayer.play()
+        } else {
+            audioPlayer.stop()
+        }
+    }
+    
+    //点击播放(暂停)按钮的方法
+    func onPlay(button: CustomButton) {
+        if button.isPlay {
+            audioPlayer.play()
+        } else {
+            audioPlayer.stop()
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
