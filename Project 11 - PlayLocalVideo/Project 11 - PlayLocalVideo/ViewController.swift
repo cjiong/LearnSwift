b809850a558e436d1b60afaa88bfd295acda4aa2
cjiong <416137318@qq.com>
2016-05-23 21:46:14 +0800

    create Project 11 - PlayLocalVideo

---
+++ Project 11 - PlayLocalVideo/Project 11 - PlayLocalVideo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project 11 - PlayLocalVideo
+//
+//  Created by 陈炯 on 16/5/23.
+//  Copyright © 2016年 Jiong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
