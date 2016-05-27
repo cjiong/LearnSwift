8149661e1f7e89e03bab748bf1bdceaf4940b5db
cjiong <416137318@qq.com>
2016-05-27 23:43:41 +0800

    complete Project 13 - MyLocation

--- Project 13 - MyLocation/Project 13 - MyLocation/ViewController.swift
+++ Project 13 - MyLocation/Project 13 - MyLocation/ViewController.swift
@@ -7,19 +7,75 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    var locationManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
+    
+    //MARK: 点击按钮，显示定位
+    @IBAction func showMyLocationButtonDidTouch(sender: AnyObject) {
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        //MARK: 属性 自动暂停位置更新
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        self.locationLabel.text = "Error while updating location" + error.localizedDescription
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        CLGeocoder().reverseGeocodeLocation(manager.location!, completionHandler:  { (placeMarks, error) -> Void in
+            
+            if (error != nil) {
+                self.locationLabel.text = "Reverse geocoder failed with error" + (error!.localizedDescription)
+                return
+            }
+            
+            if placeMarks!.count > 0 {
+                let PM = placeMarks![0]
+                self.showLocationInfo(PM)
+            } else {
+                self.locationLabel.text = "Problem with the data received from geocoder"
+            }
+        })
+    }
+    
+    //MARK: 显示位置
+    func showLocationInfo(placemark: CLPlacemark?) {
+        
+        if let containsPlacemark = placemark {
+            
+            locationManager.startUpdatingLocation()
+            
+            let locality = (containsPlacemark.locality != nil) ? containsPlacemark.locality : ""
+            
+            let postalCode = (containsPlacemark.postalCode != nil) ? containsPlacemark.postalCode : ""
+            let administrativeArea = (containsPlacemark.administrativeArea != nil) ? containsPlacemark.administrativeArea : ""
+            let country = (containsPlacemark.country != nil) ? containsPlacemark.country : ""
+            
+            self.locationLabel.text = locality! + " " + postalCode! + " " + administrativeArea! + " " + country!
+        }
+        
+    }
+    
 }
 
