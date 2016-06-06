1b02dc28b3975132a2850e94c504b9c61774eafe
cjiong <416137318@qq.com>
2016-06-06 19:49:06 +0800

    create Project 20 - BasicGesture

---
+++ Project 20 - BasicGesture/Project 20 - BasicGesture/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Project 20 - BasicGesture
+//
+//  Created by 陈炯 on 16/6/6.
+//  Copyright © 2016年 Jiong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
---
+++ Project 20 - BasicGesture/Project 20 - BasicGestureTests/Project_20___BasicGestureTests.swift
@@ -0,0 +1,36 @@
+//
+//  Project_20___BasicGestureTests.swift
+//  Project 20 - BasicGestureTests
+//
+//  Created by 陈炯 on 16/6/6.
+//  Copyright © 2016年 Jiong. All rights reserved.
+//
+
+import XCTest
+@testable import Project_20___BasicGesture
+
+class Project_20___BasicGestureTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
