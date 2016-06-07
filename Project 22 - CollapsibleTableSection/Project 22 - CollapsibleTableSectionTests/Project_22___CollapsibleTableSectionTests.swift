c1dcb21e3d2c6155c2fdec1e5f701a2ce4fe1713
cjiong <416137318@qq.com>
2016-06-07 22:22:03 +0800

    create Project 22 - CollapsibleTableSection

---
+++ Project 22 - CollapsibleTableSection/Project 22 - CollapsibleTableSectionTests/Project_22___CollapsibleTableSectionTests.swift
@@ -0,0 +1,36 @@
+//
+//  Project_22___CollapsibleTableSectionTests.swift
+//  Project 22 - CollapsibleTableSectionTests
+//
+//  Created by 陈炯 on 16/6/7.
+//  Copyright © 2016年 Jiong. All rights reserved.
+//
+
+import XCTest
+@testable import Project_22___CollapsibleTableSection
+
+class Project_22___CollapsibleTableSectionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
