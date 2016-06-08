ddbe3bc86b6e9bb36e5abe932675e8bb399ed04f
cjiong <416137318@qq.com>
2016-06-08 23:23:12 +0800

    complete storyboard

--- Project 22 - CollapsibleTableSection/Project 22 - CollapsibleTableSection/ViewController.swift
+++ Project 22 - CollapsibleTableSection/Project 22 - CollapsibleTableSection/ViewController.swift
@@ -8,16 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
