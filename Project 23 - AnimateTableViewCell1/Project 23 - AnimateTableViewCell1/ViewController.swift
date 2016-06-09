df5355dc0f1abe3a70af7eeca46bc30cf2ef0add
cjiong <416137318@qq.com>
2016-06-09 15:49:32 +0800

    create  Project 23 - AnimateTableViewCell1

---
+++ Project 23 - AnimateTableViewCell1/Project 23 - AnimateTableViewCell1/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Project 23 - AnimateTableViewCell1
+//
+//  Created by 陈炯 on 16/6/9.
+//  Copyright © 2016年 Jiong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
