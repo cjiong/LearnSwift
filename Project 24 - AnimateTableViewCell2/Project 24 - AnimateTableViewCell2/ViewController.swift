013419b3c1bab24211fc824f03f16e47c88d4793
cjiong <416137318@qq.com>
2016-06-11 22:28:59 +0800

    create Project 24 - AnimateTableViewCell2

---
+++ Project 24 - AnimateTableViewCell2/Project 24 - AnimateTableViewCell2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project 24 - AnimateTableViewCell2
+//
+//  Created by 陈炯 on 16/6/11.
+//  Copyright © 2016年 Jiong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Project 24 - AnimateTableViewCell2/Project 24 - AnimateTableViewCell2Tests/Project_24___AnimateTableViewCell2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Project_24___AnimateTableViewCell2Tests.swift
+//  Project 24 - AnimateTableViewCell2Tests
+//
+//  Created by 陈炯 on 16/6/11.
+//  Copyright © 2016年 Jiong. All rights reserved.
+//
+
+import XCTest
+@testable import Project_24___AnimateTableViewCell2
+
+class Project_24___AnimateTableViewCell2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
