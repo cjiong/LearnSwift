1de28e86d25fd149346b2eac6e68f5f76f0ab020
cjiong <416137318@qq.com>
2016-06-21 00:21:07 +0800

    create Project 27 - TableViewIndex

---
+++ Project 27 - TableViewIndex/Project 27 - TableViewIndex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project 27 - TableViewIndex
+//
+//  Created by 陈炯 on 16/6/21.
+//  Copyright © 2016年 Jiong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
