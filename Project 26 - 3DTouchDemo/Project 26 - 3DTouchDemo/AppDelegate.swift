1fbb7d4660432778da74d2196709b939aed43972
cjiong <416137318@qq.com>
2016-06-20 23:43:40 +0800

    complete Project 26 - 3DTouchDemo

--- Project 26 - 3DTouchDemo/Project 26 - 3DTouchDemo/AppDelegate.swift
+++ Project 26 - 3DTouchDemo/Project 26 - 3DTouchDemo/AppDelegate.swift
@@ -10,14 +10,96 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
+    enum ShortcutIdentifier: String {
+        
+        case First
+        case Second
+        case Third
+        
+        init?(fullType: String) {
+            guard let last = fullType.componentsSeparatedByString(".").last else { return nil }
+            
+            self.init(rawValue: last)
+        }
+        
+        var type: String {
+            return NSBundle.mainBundle().bundleIdentifier! + ".\(self.rawValue)"
+        }
+        
+    }
+    
     var window: UIWindow?
+ 
+    var launchedShortCutItem: UIApplicationShortcutItem?
+    
+    func handleShortCutItem(shortcutItem: UIApplicationShortcutItem) -> Bool {
+        
+        var handled = false
+        
+        guard ShortcutIdentifier(fullType: shortcutItem.type) != nil else { return false }
+        
+        guard let shortCutType = shortcutItem.type as String? else { return false }
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        var vc = UIViewController()
+        
+        switch (shortCutType) {
+            
+         case ShortcutIdentifier.First.type:
 
+            vc = storyboard.instantiateViewControllerWithIdentifier("SceneryVC") as! SceneryViewController
+            handled = true
+             break
+            
+         case ShortcutIdentifier.Second.type:
 
+            vc = storyboard.instantiateViewControllerWithIdentifier("CarVC") as! CarViewController
+            handled = true
+             break
+            
+         case ShortcutIdentifier.Third.type:
+
+            vc = storyboard.instantiateViewControllerWithIdentifier("CartoonVC") as! CartoonViewController
+            handled = true
+             break
+            
+         default:
+             break
+ 
+        }
+        
+        window!.rootViewController?.presentViewController(vc, animated: true, completion: nil)
+        
+        return handled
+
+    }
+
+    func application(application: UIApplication, performActionForShortcutItem shortcutItem: UIApplicationShortcutItem, completionHandler: (Bool) -> Void) {
+        
+        let handledShortCutItem = handleShortCutItem(shortcutItem)
+        completionHandler(handledShortCutItem)
+    }
+    
+    func applicationDidBecomeActive(application: UIApplication) {
+        guard let shortcut = launchedShortCutItem else { return }
+        
+        handleShortCutItem(shortcut)
+        launchedShortCutItem = nil
+        
+    }
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        if let shortcutItem = launchOptions?[UIApplicationLaunchOptionsShortcutItemKey] as? UIApplicationShortcutItem {
+            launchedShortCutItem = shortcutItem
+        }
+        
         return true
     }
+
+
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -31,10 +113,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
     func applicationWillTerminate(application: UIApplication) {
