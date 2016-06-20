9314979b08d6d216527676758ae8e37ab76043fb
cjiong <416137318@qq.com>
2016-06-20 23:14:27 +0800

    complete storyboard

---
+++ Project 26 - 3DTouchDemo/Project 26 - 3DTouchDemo/CarViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CarViewController.swift
+//  Project 26 - 3DTouchDemo
+//
+//  Created by 陈炯 on 16/6/20.
+//  Copyright © 2016年 Jiong. All rights reserved.
+//
+
+import UIKit
+
+class CarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
