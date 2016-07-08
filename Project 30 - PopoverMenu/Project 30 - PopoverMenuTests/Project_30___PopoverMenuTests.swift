eaa1c8bd7a12c651bbb9f63bfae16161cb4d8854
cjiong <416137318@qq.com>
2016-07-08 23:47:24 +0800

    create Project 30 - PopoverMenu

---
+++ Project 30 - PopoverMenu/Project 30 - PopoverMenuTests/Project_30___PopoverMenuTests.swift
@@ -0,0 +1,36 @@
+//
+//  Project_30___PopoverMenuTests.swift
+//  Project 30 - PopoverMenuTests
+//
+//  Created by 陈炯 on 16/7/8.
+//  Copyright © 2016年 Jiong. All rights reserved.
+//
+
+import XCTest
+@testable import Project_30___PopoverMenu
+
+class Project_30___PopoverMenuTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
