5a0903e35b700e7c079409fc36ad1fa9beec67f7
cjiong <416137318@qq.com>
2016-07-15 18:05:14 +0800

    create Project 37 - CoreData

---
+++ Project 37 - CoreData/Project 37 - CoreDataTests/Project_37___CoreDataTests.swift
@@ -0,0 +1,36 @@
+//
+//  Project_37___CoreDataTests.swift
+//  Project 37 - CoreDataTests
+//
+//  Created by 陈炯 on 16/7/14.
+//  Copyright © 2016年 Jiong. All rights reserved.
+//
+
+import XCTest
+@testable import Project_37___CoreData
+
+class Project_37___CoreDataTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
