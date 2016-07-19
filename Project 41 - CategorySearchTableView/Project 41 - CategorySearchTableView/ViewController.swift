a9d9855ef7fc8fc7f9cfe546ea02fe723aedddd2
cjiong <416137318@qq.com>
2016-07-19 23:34:08 +0800

    create Project 41 - CategorySearchTableView

---
+++ Project 41 - CategorySearchTableView/Project 41 - CategorySearchTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project 41 - CategorySearchTableView
+//
+//  Created by 陈炯 on 16/7/19.
+//  Copyright © 2016年 Jiong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Project 41 - CategorySearchTableView/Project 41 - CategorySearchTableViewTests/Project_41___CategorySearchTableViewTests.swift
@@ -0,0 +1,36 @@
+//
+//  Project_41___CategorySearchTableViewTests.swift
+//  Project 41 - CategorySearchTableViewTests
+//
+//  Created by 陈炯 on 16/7/19.
+//  Copyright © 2016年 Jiong. All rights reserved.
+//
+
+import XCTest
+@testable import Project_41___CategorySearchTableView
+
+class Project_41___CategorySearchTableViewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
