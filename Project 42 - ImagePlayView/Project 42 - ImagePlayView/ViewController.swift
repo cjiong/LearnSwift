a6cd22e8569d7caa586a56153f5939dd1a630e17
cjiong <416137318@qq.com>
2016-07-20 23:40:19 +0800

    create Project 42 - ImagePlayView

---
+++ Project 42 - ImagePlayView/Project 42 - ImagePlayView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project 42 - ImagePlayView
+//
+//  Created by 陈炯 on 16/7/20.
+//  Copyright © 2016年 Jiong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
