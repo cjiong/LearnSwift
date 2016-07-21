5723eb04aadf97f170855d9d5940b6666d54c166
cjiong <416137318@qq.com>
2016-07-21 22:39:13 +0800

    create Project 43 - LaunchScreenCountdown

---
+++ Project 43 - LaunchScreenCountdown/Project 43 - LaunchScreenCountdownTests/Project_43___LaunchScreenCountdownTests.swift
@@ -0,0 +1,36 @@
+//
+//  Project_43___LaunchScreenCountdownTests.swift
+//  Project 43 - LaunchScreenCountdownTests
+//
+//  Created by 陈炯 on 16/7/21.
+//  Copyright © 2016年 Jiong. All rights reserved.
+//
+
+import XCTest
+@testable import Project_43___LaunchScreenCountdown
+
+class Project_43___LaunchScreenCountdownTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
