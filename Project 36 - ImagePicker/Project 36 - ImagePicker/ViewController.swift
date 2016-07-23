7b894acf0b8c14218e78f7d92401cf1de1d0f80f
cjiong <416137318@qq.com>
2016-07-23 12:11:58 +0800

    complete Project 36 - ImagePicker

--- Project 36 - ImagePicker/Project 36 - ImagePicker/ViewController.swift
+++ Project 36 - ImagePicker/Project 36 - ImagePicker/ViewController.swift
@@ -10,17 +10,42 @@ import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
-    @IBOutlet weak var headPicture: UIButton!
+    @IBOutlet weak var headPicture: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        headPicture.layer.masksToBounds = true
+        
+        //MARK: 给imageView添加点击事件
+        let gestureRecognizer = UITapGestureRecognizer()
+        gestureRecognizer.addTarget(self, action: Selector("changeHeadPicture"))
+        headPicture.addGestureRecognizer(gestureRecognizer)
     }
     
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
     
-    @IBAction func changeHeadPicture(sender: AnyObject) {
+    func changeHeadPicture() {
+        
+        let userIconAlert = UIAlertController(title: "请选择", message: "", preferredStyle: UIAlertControllerStyle.ActionSheet)
+        
+        let chooseFromPhotoalbum = UIAlertAction(title: "从相册选择", style: UIAlertActionStyle.Default, handler: chooseFromPhotoAlbum)
+        userIconAlert.addAction(chooseFromPhotoalbum)
+        
+        let chooseFromcamera = UIAlertAction(title: "拍照", style: UIAlertActionStyle.Default,handler: chooseFromCamera)
+        userIconAlert.addAction(chooseFromcamera)
+        
+        let cancelAction = UIAlertAction(title: "取消", style: UIAlertActionStyle.Cancel, handler: nil)
+        userIconAlert.addAction(cancelAction)
+        
+        self.presentViewController(userIconAlert, animated: true, completion: nil)
+    
+        
+    }
+    
+    func chooseFromPhotoAlbum(alertAction: UIAlertAction) -> Void {
         
         if UIImagePickerController.isSourceTypeAvailable(.PhotoLibrary) {
             
@@ -31,24 +56,50 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             picker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
             picker.allowsEditing = true
             
-            self.presentViewController(picker, animated: true, completion: { () -> Void in
-                
-            })
+            self.presentViewController(picker, animated: true, completion: nil)
             
         } else {
             
             print("error")
         }
+        
     }
 
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
-        
-        headPicture.setBackgroundImage(image, forState: UIControlState.Normal)
-        picker.dismissViewControllerAnimated(true) { () -> Void in
+    func chooseFromCamera(alertAction: UIAlertAction) -> Void {
+     
+        if UIImagePickerController.isSourceTypeAvailable(.Camera) {
             
+            let picker = UIImagePickerController()
+            
+            //MARK: 设置代理
+            picker.delegate = self
+            picker.sourceType = UIImagePickerControllerSourceType.Camera
+            picker.allowsEditing = true
+            
+            self.presentViewController(picker, animated: true, completion: nil)
+            
+        } else {
+            
+            print("error")
         }
 
     }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        
+        let image = info[UIImagePickerControllerEditedImage] as! UIImage
+        
+        let rect = CGRect(x: 292, y: 268, width: 77, height: 75)
+        headPicture.frame = rect
+        headPicture.image = image
+        picker.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        
+        picker.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
