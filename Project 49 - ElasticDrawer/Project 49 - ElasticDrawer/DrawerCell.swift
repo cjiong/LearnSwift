265fee36db06fe2c94d6d7a8155f9cbfdd4d4d47
cjiong <416137318@qq.com>
2016-08-23 01:00:18 +0800

    complete storyboard

---
+++ Project 49 - ElasticDrawer/Project 49 - ElasticDrawer/DrawerCell.swift
@@ -0,0 +1,22 @@
+//
+//  DrawerCell.swift
+//  Project 49 - ElasticDrawer
+//
+//  Created by 陈炯 on 16/8/23.
+//  Copyright © 2016年 Jiong. All rights reserved.
+//
+
+import UIKit
+
+class DrawerCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+}
