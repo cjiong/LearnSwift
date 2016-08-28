59ecc6eac8f353bf12ecb7adb43c0755442f2622
cjiong <416137318@qq.com>
2016-08-28 22:12:19 +0800

    complete Project 51 - LuanchPageSplash

--- Project 51 - LuanchPageSplash/Project 51 - LuanchPageSplash/AppDelegate.swift
+++ Project 51 - LuanchPageSplash/Project 51 - LuanchPageSplash/AppDelegate.swift
@@ -12,10 +12,35 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    var mask: CALayer?
+    var imageView: UIImageView?
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        self.window?.rootViewController = UIViewController()
+        
+        let imageView = UIImageView(frame: self.window!.frame)
+        imageView.image = UIImage(named: "james")
+        self.window!.addSubview(imageView)
+        
+        self.mask = CALayer()
+        self.mask!.contents = UIImage(named: "swift")?.CGImage
+        self.mask!.contentsGravity = kCAGravityResizeAspect
+        self.mask!.bounds = CGRect(x: 0, y: 0, width: 100, height: 81)
+        self.mask!.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        self.mask!.position = CGPoint(x: imageView.frame.size.width / 2, y: imageView.frame.size.height / 2)
+        imageView.layer.mask = mask
+        self.imageView = imageView
+        
+        //MARK: 开始动画
+        animateMask()
+        
+        self.window?.backgroundColor = UIColor.whiteColor()
+        self.window?.makeKeyAndVisible()
+        //MARK: 隐藏状态栏
+        UIApplication.sharedApplication().statusBarHidden = true
+        
         return true
     }
 
@@ -41,6 +66,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+
+    func animateMask() {
+        
+        let keyFrameAnimation = CAKeyframeAnimation(keyPath: "bounds")
+        keyFrameAnimation.delegate = self
+        keyFrameAnimation.duration = 0.6
+        keyFrameAnimation.beginTime = CACurrentMediaTime() + 0.5
+        keyFrameAnimation.timingFunctions = [CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut), CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)]
+        let initalBounds = NSValue(CGRect: mask!.bounds)
+        let secondBounds = NSValue(CGRect: CGRect(x: 0, y: 0, width: 90, height: 73))
+        let thridBounds = NSValue(CGRect: CGRect(x: 0, y: 0, width: 800, height: 146))
+        let finalBounds = NSValue(CGRect: CGRect(x: 0, y: 0, width: 1600, height: 1300))
+        keyFrameAnimation.values = [initalBounds, secondBounds, thridBounds, finalBounds]
+        keyFrameAnimation.keyTimes = [0, 0.3, 0.6, 1]
+        self.mask!.addAnimation(keyFrameAnimation, forKey: "bounds")
+    }
+
+    override func animationDidStop(anim: CAAnimation, finished flag: Bool) {
+        self.imageView?.layer.mask = nil
+    }
 
 }
 
