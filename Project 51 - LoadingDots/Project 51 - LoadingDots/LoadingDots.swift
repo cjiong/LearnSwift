fca44502d11577e7cf3902dad9c2db5d421635a4
cjiong <416137318@qq.com>
2016-08-28 20:06:11 +0800

    add LoadingDots.xib

---
+++ Project 51 - LoadingDots/Project 51 - LoadingDots/LoadingDots.swift
@@ -0,0 +1,14 @@
+//
+//  LoadingDots.swift
+//  Project 51 - LoadingDots
+//
+//  Created by 陈炯 on 16/8/28.
+//  Copyright © 2016年 Jiong. All rights reserved.
+//
+
+import UIKit
+
+class LoadingDots: UIView {
+
+    
+}
