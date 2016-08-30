9a7e5feac026b59b510f84107e3e2d85fc7c4c49
cjiong <416137318@qq.com>
2016-08-30 20:36:33 +0800

    create Project 51 - MusicIndicator

---
+++ Project 52 - MusicIndicator/Project 52 - MusicIndicatorTests/Project_52___MusicIndicatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  Project_52___MusicIndicatorTests.swift
+//  Project 52 - MusicIndicatorTests
+//
+//  Created by 陈炯 on 16/8/30.
+//  Copyright © 2016年 Jiong. All rights reserved.
+//
+
+import XCTest
+@testable import Project_52___MusicIndicator
+
+class Project_52___MusicIndicatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
