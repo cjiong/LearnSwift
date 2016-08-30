e9f7af53dc27bd013267730026a27df641da1a53
cjiong <416137318@qq.com>
2016-08-30 21:08:24 +0800

    complete storyboard

---
+++ Project 52 - MusicIndicator/Project 52 - MusicIndicator/SongCell.swift
@@ -0,0 +1,22 @@
+//
+//  SongCell.swift
+//  Project 52 - MusicIndicator
+//
+//  Created by 陈炯 on 16/8/30.
+//  Copyright © 2016年 Jiong. All rights reserved.
+//
+
+import UIKit
+
+class SongCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
