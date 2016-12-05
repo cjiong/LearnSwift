41ec90f267ce57a8e088b561794f6692d1a23892
cjiong <416137318@qq.com>
2016-12-06 00:07:18 +0800

    swift 3.0 version

--- Project 51 - LuanchPageSplash/Project 51 - LuanchPageSplash/AppDelegate.swift
+++ Project 51 - LuanchPageSplash/Project 51 - LuanchPageSplash/AppDelegate.swift
@@ -9,15 +9,15 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, CAAnimationDelegate {
 
     var window: UIWindow?
     var mask: CALayer?
     var imageView: UIImageView?
     
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        self.window = UIWindow(frame: UIScreen.main.bounds)
         self.window?.rootViewController = UIViewController()
         
         let imageView = UIImageView(frame: self.window!.frame)
@@ -25,7 +25,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         self.window!.addSubview(imageView)
         
         self.mask = CALayer()
-        self.mask!.contents = UIImage(named: "swift")?.CGImage
+        self.mask!.contents = UIImage(named: "swift")?.cgImage
         self.mask!.contentsGravity = kCAGravityResizeAspect
         self.mask!.bounds = CGRect(x: 0, y: 0, width: 100, height: 81)
         self.mask!.anchorPoint = CGPoint(x: 0.5, y: 0.5)
@@ -36,33 +36,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         //MARK: 开始动画
         animateMask()
         
-        self.window?.backgroundColor = UIColor.whiteColor()
+        self.window?.backgroundColor = UIColor.white
         self.window?.makeKeyAndVisible()
         //MARK: 隐藏状态栏
-        UIApplication.sharedApplication().statusBarHidden = true
+        UIApplication.shared.isStatusBarHidden = true
         
         return true
     }
 
-    func applicationWillResignActive(application: UIApplication) {
+    func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
 
-    func applicationDidEnterBackground(application: UIApplication) {
+    func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
 
-    func applicationWillEnterForeground(application: UIApplication) {
+    func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
 
-    func applicationDidBecomeActive(application: UIApplication) {
+    func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
-    func applicationWillTerminate(application: UIApplication) {
+    func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
@@ -74,16 +74,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         keyFrameAnimation.duration = 0.6
         keyFrameAnimation.beginTime = CACurrentMediaTime() + 0.5
         keyFrameAnimation.timingFunctions = [CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut), CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)]
-        let initalBounds = NSValue(CGRect: mask!.bounds)
-        let secondBounds = NSValue(CGRect: CGRect(x: 0, y: 0, width: 90, height: 73))
-        let thridBounds = NSValue(CGRect: CGRect(x: 0, y: 0, width: 800, height: 146))
-        let finalBounds = NSValue(CGRect: CGRect(x: 0, y: 0, width: 1600, height: 1300))
+        let initalBounds = NSValue(cgRect: mask!.bounds)
+        let secondBounds = NSValue(cgRect: CGRect(x: 0, y: 0, width: 90, height: 73))
+        let thridBounds = NSValue(cgRect: CGRect(x: 0, y: 0, width: 800, height: 146))
+        let finalBounds = NSValue(cgRect: CGRect(x: 0, y: 0, width: 1600, height: 1300))
         keyFrameAnimation.values = [initalBounds, secondBounds, thridBounds, finalBounds]
         keyFrameAnimation.keyTimes = [0, 0.3, 0.6, 1]
-        self.mask!.addAnimation(keyFrameAnimation, forKey: "bounds")
+        self.mask!.add(keyFrameAnimation, forKey: "bounds")
     }
 
-    override func animationDidStop(anim: CAAnimation, finished flag: Bool) {
+    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
         self.imageView?.layer.mask = nil
     }
 
