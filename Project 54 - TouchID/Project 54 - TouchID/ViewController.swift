57426af3c97b3cd2f4662cb09162d866bcc291e2
cjiong <416137318@qq.com>
2017-09-29 20:58:11 +0800

    create Project 54 - TouchID

---
+++ Project 54 - TouchID/Project 54 - TouchID/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project 54 - TouchID
+//
+//  Created by 陈炯 on 2017/9/29.
+//  Copyright © 2017年 陈炯. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Project 54 - TouchID/Project 54 - TouchIDTests/Project_54___TouchIDTests.swift
@@ -0,0 +1,30 @@
+//___FILEHEADER___
+
+import XCTest
+@testable import ___VARIABLE_productName:identifier___
+
+class ___FILEBASENAMEASIDENTIFIER___: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
